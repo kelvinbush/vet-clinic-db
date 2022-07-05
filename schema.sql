e40d7d219d41125c348bc3ff283f069b0fec0949
Kelvin Bush <kelybush@gmail.com>
2022-07-05 05:43:11 +0300

    create animals table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
-
+CREATE TABLE animals
+(
+    id              INT PRIMARY KEY NOT NULL,
+    name            VARCHAR(25),
+    date_of_birth   DATE            NOT NULL DEFAULT CURRENT_DATE,
+    escape_attempts INT,
+    neutered        BOOLEAN,
+    weight_kg       DECIMAL
+);
