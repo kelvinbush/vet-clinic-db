618f9d08f7943cf46be0d39e0110a5c3cc063145
Kelvin Bush <kelybush@gmail.com>
2022-07-05 05:52:46 +0300

    auto generate the primary key

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE vet_clinic;
 CREATE TABLE animals
 (
-    id              INT PRIMARY KEY NOT NULL,
+    id              SERIAL PRIMARY KEY,
     name            VARCHAR(25),
     date_of_birth   DATE            NOT NULL DEFAULT CURRENT_DATE,
     escape_attempts INT,
