ae6e8f56c0783340e15ede054d4159da490fd468
Kelvin Bush <kelybush@gmail.com>
2022-07-05 09:06:27 +0300

    play with transactions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,29 @@ SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name NOT LIKE 'Gabumon';
 SELECT * from animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals set species = 'unspecified' where species IS NULL;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals set species = 'digimon' where name LIKE '%mon';
+COMMIT;
+
+BEGIN;
+UPDATE animals set species = 'pokemon' where species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE from animals;
+ROLLBACK;
+
+BEGIN;
+DELETE from animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete_savepoint;
+UPDATE animals set weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT delete_savepoint;
+UPDATE animals set weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+/* Queries to answer questions below*/
